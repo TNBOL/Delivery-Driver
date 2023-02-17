7ad3768a061bffe5eddbaf907ae0f30a6aaca2f5
Thomas NIMMO <116820067+TNBOL@users.noreply.github.com>
2023-02-17 21:45:47 +0100

    Implemented If Statements and Tags

---
+++ Assets/Delivery.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision!"); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package")
+
+        {
+            Debug.Log("Package picked up");
+        }
+
+        if (other.tag == "Customer") ;
+
+        {
+            Debug.Log("Package Delivered");
+        }
+            
+    }
+
+}
