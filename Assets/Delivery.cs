85784e70629aca942925776fe613a080d99fc743
Thomas NIMMO <116820067+TNBOL@users.noreply.github.com>
2023-02-18 13:26:46 +0100

    Added visual feedback with GetComponent

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,6 +28,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
 
@@ -27,6 +37,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package Delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
             
     }
